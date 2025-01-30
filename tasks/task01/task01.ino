e7d75b2238654c09dedf9d59cc668017dbb8f409
yuuusha <92097716+yuuusha@users.noreply.github.com>
2025-01-30 15:52:44 +0400

    Update task01.ino

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,29 +1,44 @@
+struct Pin {
+  volatile uint8_t* port_type;
+  uint8_t offset;
+};
+
 int N = 5;
-int leds[] = {3, 5, 6, 9, 10};
-double intervals[] = {10, 1, 0.5, 0.1, 0.05};
+
+Pin leds[] = {
+  { &PORTD, (1 << PD3) },
+  { &PORTD, (1 << PD5) },
+  { &PORTD, (1 << PD6) },
+  { &PORTB, (1 << PD1) },
+  { &PORTB, (1 << PD2) },
+};
+
+double intervals[] = {10000, 1000, 500, 100, 50};
 int prev[] = {0, 0, 0, 0, 0};
 int states[] = {0, 0, 0, 0, 0};
 
 void setup() {
   for (int i = 0; i < N; i++) {
-    pinMode(leds[i], OUTPUT);
+    volatile uint8_t* ddr = (volatile uint8_t*)(leds[i].port_type - 1);
+    *ddr |= leds[i].offset;
   }
 }
 
 void loop() {
   for (int i = 0; i < N; i++) {
-      unsigned long currentMillis = millis();
+      unsigned long currentMicros = micros();
 
-    if (currentMillis - prev[i] >= intervals[i]) {
-      prev[i] = currentMillis;
+    if (currentMicros - prev[i] >= intervals[i]) {
 
-      if (states[i] == 0) {
-        states[i] = 1;
+
+      if (states[i]) {
+        *leds[i].port_type &= ~leds[i].offset;
       } else {
-        states[i] = 0;
+        *leds[i].port_type |= leds[i].offset;
       }
 
-      digitalWrite(leds[i], states[i]);
+      states[i] ^= 1;
+      prev[i] = currentMicros;
     }
   }
 }
