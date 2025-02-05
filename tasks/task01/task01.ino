28637ccdcf8536cc2a3240f54c6677f06f992a6c
yuuusha <92097716+yuuusha@users.noreply.github.com>
2025-02-05 16:19:16 +0400

    Update task01.ino

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,44 +1,43 @@
-struct Pin {
-  volatile uint8_t* port_type;
-  uint8_t offset;
-};
+unsigned int delay0 = 10000;
+unsigned int delay1 = 1000;
+unsigned int delay2 = 500;
+unsigned int delay3 = 100;
+unsigned int delay4 = 50;
 
-int N = 5;
-
-Pin leds[] = {
-  { &PORTD, (1 << PD3) },
-  { &PORTD, (1 << PD5) },
-  { &PORTD, (1 << PD6) },
-  { &PORTB, (1 << PD1) },
-  { &PORTB, (1 << PD2) },
-};
-
-unsigned long intervals[] = {10000, 1000, 500, 100, 50};
-int prev[] = {0, 0, 0, 0, 0};
-int states[] = {0, 0, 0, 0, 0};
+unsigned int previous0 = 0;
+unsigned int previous1 = 0;
+unsigned int previous2 = 0;
+unsigned int previous3 = 0;
+unsigned int previous4 = 0;
 
 void setup() {
-  for (int i = 0; i < N; i++) {
-    volatile uint8_t* ddr = (volatile uint8_t*)(leds[i].port_type - 1);
-    *ddr |= leds[i].offset;
-  }
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
-void loop() {
-  for (int i = 0; i < N; i++) {
-      unsigned long currentMicros = micros();
-
-    if (currentMicros - prev[i] >= intervals[i]) {
-
-
-      if (states[i]) {
-        *leds[i].port_type &= ~leds[i].offset;
-      } else {
-        *leds[i].port_type |= leds[i].offset;
-      }
-
-      states[i] ^= 1;
-      prev[i] = currentMicros;
-    }
-  }
+void loop() { 
+  unsigned int currentMicros = micros();
+  if (currentMicros - previous0 >= delay0) {
+      PORTD ^= (1 << PD3);
+      previous0 = currentMicros;
+  } 
+  if (currentMicros - previous1 >= delay1) {
+      PORTD ^= (1 << PD5);
+      previous1 = currentMicros;
+  } 
+  if (currentMicros - previous2 >= delay2) {
+      PORTD ^= (1 << PD6);
+      previous2 = currentMicros;
+  } 
+  if (currentMicros - previous3 >= delay3) {
+      PORTB ^= (1 << PB1);
+      previous3 = currentMicros;
+  } 
+  if (currentMicros - previous4 >= delay4) {
+      PORTB ^= (1 << PB2);
+      previous4 = currentMicros;
+  } 
 }
